6d98a92ba99d3ad50c0170a462264caed94239ca
Leandro Velasquez <ct351463@gmail.com>
2022-10-13 16:57:54 -0300

    Definimos la ruta get /registrar en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', IndexController::class);
+
+Route::get('/registrar', [RegisterController::class, 'index']);
