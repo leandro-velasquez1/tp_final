c8359fc8b8fc600531133655950d4cb59e066d7c
Leandro Velasquez <ct351463@gmail.com>
2022-10-14 14:34:49 -0300

    Reemplazamos el metodo index por create en RegisterController, tambien definimos el metodo store, usamos una instancia del model PageUser para almacenar un registro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PageUser;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function index() {
+    public function create() {
         return view('usersApi.register');
+    }
+
+    public function store(Request $request) {
+        $pageUser = new PageUser();
+        $pageUser->username = $request->input('username');
+        $pageUser->email = $request->input('email');
+        $pageUser->password = $request->input('password');
+        $pageUser->save();
+        return '';
     }
 }
