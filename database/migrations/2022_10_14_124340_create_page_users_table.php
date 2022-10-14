a34c223aae815f1057288b838267cbae43329bbb
Leandro Velasquez <ct351463@gmail.com>
2022-10-14 17:20:19 -0300

    Creamos la migracion create_page_users_table

---
+++ database/migrations/2022_10_14_124340_create_page_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 100);
+            $table->string('email', 255);
+            $table->string('password', 120);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_users');
+    }
+};
