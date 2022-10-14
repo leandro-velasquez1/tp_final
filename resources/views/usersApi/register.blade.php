ab2e5727d30b26353bf5cd63e0751af1bf7a49c8
Leandro Velasquez <ct351463@gmail.com>
2022-10-14 17:45:08 -0300

    Le agregamos el atributo class a todas las etiquetas de la vista register

--- resources/views/usersApi/register.blade.php
+++ resources/views/usersApi/register.blade.php
@@ -2,24 +2,36 @@
 @section('title') Registrar @endsection
 
 @section('content')
-    <h2>Registrar</h2>
-    <form action="">
-        <div>
-            <label for="">Username</label>
-            <input type="text" name="username">
+    <section class="section-register">
+        <h2 class="section-register__title">Registrar</h2>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="section-register__list-errors">
+                @foreach ($errors->all() as $error)
+                    <li class="section-register__item-error">{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        <div>
-            <label for="">Email</label>
-            <input type="email" name="email">
-        </div>
-        <div>
-            <label for="">Password</label>
-            <input type="password" name="password">
-        </div>
-        <div>
-            <label for="">Repeat password</label>
-            <input type="password" name="repeat_password">
-        </div>
-        <button>Registrar</button>
-    </form>
+        @endif
+        <form class="form-register" action="/registrar" method="POST">
+            @csrf
+            <div class="form-register__div">
+                <label class="form-register__label" for="">Username</label>
+                <input class="form-register__input" type="text" name="username">
+            </div>
+            <div class="form-register__div">
+                <label class="form-register__label" for="">Email</label>
+                <input class="form-register__input" type="email" name="email">
+            </div>
+            <div class="form-register__div">
+                <label class="form-register__label" for="">Password</label>
+                <input class="form-register__input" type="password" name="password">
+            </div>
+            <div class="form-register__div">
+                <label class="form-register__label" for="">Repeat password</label>
+                <input class="form-register__input" type="password" name="password_confirmation">
+            </div>
+            <button class="form-register__button">Registrar</button>
+        </form>
+    </section class="section-register">
 @endsection
