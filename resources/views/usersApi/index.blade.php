f51a939aced378fdc50a307f92efcc161c9234ed
Leandro Velasquez <ct351463@gmail.com>
2022-10-17 10:17:57 -0300

    Encerramos toda la estructura de la pagina dentro de una etiqueta section, agregamos algunas clases para dar estilos, tambien utilizamos la directiva condicional if de blade para mostrar un mensaje

--- resources/views/usersApi/index.blade.php
+++ resources/views/usersApi/index.blade.php
@@ -2,9 +2,17 @@
 @section('title') Inicio @endsection
 
 @section('content')
-    <section>
-        <div>
-            <div>
+    <section class="section-documentation">
+        <div class="section-documentation__container1">
+            <p>IMPORTANTE: para poder utilizar la API necesita tener un token de acceso el mismo se obtiene registrandose</p>
+            @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+            @endif
+        </div>
+        <div class="section-documentation__container2">
+            <div class="section-documentation__request-info">
                 <h3>GET</h3>
                 <div>
                     <h4>Request</h4>
@@ -17,7 +25,7 @@
                     <p>Codigos: 200 OK, 401 No autorizado</p>
                 </div>
             </div>
-            <div>
+            <div class="section-documentation__request-info">
                 <h3>POST</h3>
                 <div>
                     <h4>Request</h4>
@@ -31,7 +39,7 @@
                     <p>Codigos: 201 Usuario creado, 400 La solicitud contiene un error ,401 No autorizado</p>
                 </div>
             </div>
-            <div>
+            <div class="section-documentation__request-info">
                 <h3>PUT</h3>
                 <div>
                     <h4>Request</h4>
@@ -45,7 +53,7 @@
                     <p>Codigos: 200 OK, 400 La solicitud contiene un error , 401 No autorizado, 404 No se encontro al usuario</p>
                 </div>
             </div>
-            <div>
+            <div class="section-documentation__request-info">
                 <h3>DELETE</h3>
                 <div>
                     <h4>Request</h4>
