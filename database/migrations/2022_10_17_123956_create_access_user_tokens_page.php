6780e16796f31ce477a5897b3cfa62551b40cb7e
Leandro Velasquez <ct351463@gmail.com>
2022-10-17 14:43:07 -0300

    Creamos una migracion para crear la tabla access_user_tokens_page

---
+++ database/migrations/2022_10_17_123956_create_access_user_tokens_page.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access_user_tokens_page', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('page_user_id')->constrained('page_users');
+            $table->string('token', 120)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access_user_tokens_page');
+    }
+};
