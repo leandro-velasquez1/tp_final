3f092649b1aed974f7f90379bca023f51a8e2077
Leandro Velasquez <ct351463@gmail.com>
2022-10-18 10:40:34 -0300

    Creamos la ruta POST /persons y le asignamos el metodo store de la clase PersonController

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/persons', [PersonController::class, 'getAll']);
+Route::post('/persons', [PersonController::class, 'store']);
