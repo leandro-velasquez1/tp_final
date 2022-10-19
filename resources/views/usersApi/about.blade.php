c0faf00faaf09b65871926a4959f65a4b2d50bef
Leandro Velasquez <ct351463@gmail.com>
2022-10-19 14:30:08 -0300

    Creamos la vista about y definimos su estructura html

---
+++ resources/views/usersApi/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+@section('title') About @endsection
+
+@section('content')
+    <section class="section-about">
+        <div class="section-about__div">
+            <h2 class="section-about__title">Acerca de tp_final</h2>
+            <p class="section-about__text">API registro de personas es una aplicacion web que se encuentra dividida en 2 partes, la primera es el sitio web que contiene 4 paginas, API documentation, Generar token, Registrarse y About. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere numquam officiis ipsum, tempore dolore aliquid id expedita quas laboriosam rerum incidunt nesciunt iure sit. Perspiciatis doloremque tempore consequatur mollitia eos numquam ex, veritatis nam soluta doloribus. Dicta deleniti vel doloribus reiciendis nemo. Recusandae maxime voluptates corrupti harum veniam rem explicabo assumenda. Unde tenetur architecto ipsam quasi. Sapiente, fugit libero. Recusandae ipsum deserunt vel quas quis cumque aut quaerat, itaque similique accusamus impedit! Numquam aliquid quas doloribus sint reprehenderit sapiente alias.<a class="section-about__enlace" href="https://github.com/tp-final1/tp_final/blob/master/README.md" target="_blank">Mas informacion</a></p>
+        </div>
+    </section>
+@endsection
